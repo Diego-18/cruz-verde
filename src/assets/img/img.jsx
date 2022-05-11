17a3ee725612d96ca2a7d7af1da33619fe8974e0
Diego Chavez <ingdiegochavez18@gmail.com>
2022-05-11 10:32:22 -0500

    📦 NEW: add assets
    ** Add pictures, files json.

---
+++ src/assets/img/img.jsx
@@ -0,0 +1,15 @@
+import logo from './Cruz verde.png';
+import back from './back-buttom.svg';
+import netux from './logo-netux.png';
+import photo1 from './Grupo1376.png';
+import photo2 from './Grupo2073.svg';
+import mapa from './Mapa--blanco-1.png';
+
+export {
+    logo,
+    back,
+    netux,
+    photo1,
+    photo2,
+    mapa
+};
