87fb196154d88c9d8255fb962f9edbe76fd2fb60
Diego-18 <ingdiegochavez18@gmail.com>
2023-07-04 21:16:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  jsxInject: `import React from 'react'`,
-  plugins: [react()]
-})
+	jsxInject: `import React from 'react'`,
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			external: ["react", "react-dom"],
+		},
+	},
+});
