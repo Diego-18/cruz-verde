c2593640604c2be91df0215a52abe2d046f667b0
Diego-18 <ingdiegochavez18@gmail.com>
2023-07-04 19:25:00 -0500

    fix-desing-step1

--- src/components/Step1/Step1.jsx
+++ src/components/Step1/Step1.jsx
@@ -1,10 +1,10 @@
-import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { useStateMachine } from 'little-state-machine';
-import Navbar from '../Navbar/Navbar.jsx';
-import updateAction from '../../updateAction.jsx';
-import infoSede from '../../assets/data/info.json';
-import './Step1.css';
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useStateMachine } from "little-state-machine";
+import Navbar from "../Navbar/Navbar.jsx";
+import updateAction from "../../updateAction.jsx";
+import infoSede from "../../assets/data/info.json";
+import "./Step1.css";
 
 const Step1 = (props) => {
 	const { state, action } = useStateMachine(updateAction);
@@ -14,73 +14,68 @@ const Step1 = (props) => {
 		formState: { errors },
 	} = useForm({
 		defaultValues: state.yourDetails,
-		mode: 'onChange',
+		mode: "onChange",
 	});
 
 	const navigate = useNavigate();
 
 	const Back = (data) => {
 		action();
-		navigate('/');
+		navigate("/");
 	};
 
 	const Step2 = (data) => {
 		action(data);
-		navigate('/s2');
+		navigate("/s2");
 	};
 
 	return (
 		<div className="step-1">
 			<Navbar NavStyle="2" />
 			<div className="cstm-container">
-				<div className="row">
-					<div className="col-xs-12 col-sm-12 col-md-4 col-lg- section-left">
-						<form onSubmit={handleSubmit(Step2)}>
-							<p className="label-form">
-								Selecciona la oficina más cercana
-							</p>
-							{infoSede.map((item, index) => (
-								<div key={index}>
-									<div className="form-check">
-										<input
-											className="form-check-input"
-											type="radio"
-											{...register('sede', {
-												required:
-													'Debes seleccionar una oficina, no puede quedar vacío.',
-											})}
-											value={item.name}
-										/>
-										<label className="form-check-label">
-											{item.name}
-										</label>
+				<div className="card">
+					<form onSubmit={handleSubmit(Step2)}>
+						<p className="label-form">
+							Selecciona la oficina más cercana
+						</p>
+						{infoSede.map((item, index) => (
+							<div key={index}>
+								<div className="form-check">
+									<input
+										className="form-check-input"
+										type="radio"
+										{...register("sede", {
+											required:
+												"Debes seleccionar una oficina, no puede quedar vacío.",
+										})}
+										value={item.name}
+									/>
+									<label className="form-check-label">
+										{item.name}
+									</label>
 
-										<p>{item.location}</p>
-									</div>
-									<hr />
+									<p>{item.location}</p>
 								</div>
-							))}
-							{errors.sede && (
-								<span className="error-message">
-									Este campo es requerido.
-								</span>
-							)}
+								<hr />
+							</div>
+						))}
+						{errors.sede && (
+							<span className="error-message">
+								Este campo es requerido.
+							</span>
+						)}
 
-							<button type="submit" className="btn cstm-btn">
-								Seleccionar
-							</button>
+						<button type="submit" className="btn cstm-btn">
+							Seleccionar
+						</button>
 
-							<button
-								onClick={Back}
-								className="btn cstm-btn d-md-none d-lg-none d-xl-none"
-							>
-								Atrás
-							</button>
-						</form>
-					</div>
-					<div className="col-xs-12 col-sm-12 col-md-7 col-lg-7">
-						<div className="section-right"></div>
-					</div>
+						<button
+							onClick={Back}
+							className="btn cstm-btn d-md-none d-lg-none d-xl-none"
+						>
+							Atrás
+						</button>
+					</form>
 				</div>
 			</div>
 		</div>
